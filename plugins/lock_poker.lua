e4cf4d0c9d7d2622e793b4c06f36b8c9992fdb76
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-17 04:23:54 +0430

    Create lock_poker.lua

---
+++ plugins/lock_poker.lua
@@ -0,0 +1,21 @@
+﻿local function run(msg)
+
+    local data = load_data(_config.moderation.data)
+
+     if data[tostring(msg.to.id)]['settings']['lock_poker'] == 'yes' then
+
+
+if msg.to.type == 'channel' and not is_momod(msg) then
+	
+	del_msg(msg.from.id, msg.to.id)
+
+        return 
+      end
+   end
+end
+
+return {patterns = {
+      "🙁",
+      "😐",
+	  "😑"
+}, run = run}
