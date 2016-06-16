30fa9079c5d3b6abb237b19a6e6c473c57af698e
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-17 00:40:18 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,32 +232,32 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {85831686},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
-An advanced administration bot based on TG-CLI written in Lua
+    about_text = [[Sphero + Helper
+An advanced administration bot based on TeleSeed written in Lua
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/3pehrdev
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
+@MrBlackLife [Founder]
+@MrBlackLife [Developer]
+@MrBlackLife [Manager]
+@MrBlackLife [Admin]
+:)))))))))
+----------------
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
+MrJacki
+Mobin
+Allwen
+ImanDaneshi
+----------------
 Our channels
-@teleseedch [English]
-@iranseed [persian]
-
-Our website 
-http://teleseed.seedteam.org/
+@SpheroCh [English]
+@SpheroCh [persian]
+:)))))))))
+Open Source #Soon . . .
+Tnx To All
 ]],
     help_text_realm = [[
 Realm Commands:
