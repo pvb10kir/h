055630436fbaa60ba68a77ee4be2ba6c4482db2b
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-20 12:53:22 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -83,7 +83,6 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'Total messages : '..user_info_msgs..'\n\n'
-  text = text..'@BeyondTeam'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'id not found.\nuse : /info @username', ok_cb, false)
