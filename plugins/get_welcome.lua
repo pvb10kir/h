6640bad4a45581e3a18d6f8b5842f7b89f152a93
BlackLifeDev <sepehrsalehi011@gmail.com>
2016-06-21 17:29:05 +0430

    Update get_welcome.lua

--- plugins/get_welcome.lua
+++ plugins/get_welcome.lua
@@ -1,22 +1,59 @@
+do
 local function run(msg, matches, callback, extra)
-
 local data = load_data(_config.moderation.data)
-
-local group_welcome = data[tostring(msg.to.id)]['group_welcome']
-
-if matches[1] == 'chat_add_user' or 'chat_add_user_link' then --For Normal Group
-    if not msg.service then 
-        return nil 
-    else
-        return group_welcome
+local rules = data[tostring(msg.to.id)]['rules']
+local about = data[tostring(msg.to.id)]['description']
+local hash = 'group:'..msg.to.id
+local group_welcome = redis:hget(hash,'welcome')
+if matches[1] == 'delbye' and not matches[2] and is_owner(msg) then 
+    
+   redis:hdel(hash,'welcome')
+        return '✔Welcome Text Has Been Deleted✔'
 end
+
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+
+if matches[1] == 'setbye' and is_owner(msg) then
+redis:hset(hash,'welcome',matches[2])
+        return '🛎SuperGroup Bye Text Has Been Changed To:\n'..matches[2]
 end
+
+if matches[1] == 'chat_del_user' or 'channel_del_user' and msg.service then
+group_welcome = string.gsub(group_welcome, '{gpname}', msg.to.title)
+group_welcome = string.gsub(group_welcome, '{firstname}', ""..(msg.action.user.first_name or '').."")
+ group_welcome = string.gsub(group_welcome, '{lastname}', ""..(msg.action.user.last_name or '').."")
+  group_welcome = string.gsub(group_welcome, '{username}', "@"..(msg.action.user.username or '').."")
+  group_welcome = string.gsub(group_welcome, '{fatime}', ""..(jdat.FAtime).."")
+  group_welcome = string.gsub(group_welcome, '{entime}', ""..(jdat.ENtime).."")
+  group_welcome = string.gsub(group_welcome, '{fadate}', ""..(jdat.FAdate).."")
+  group_welcome = string.gsub(group_welcome, '{endate}', ""..(jdat.ENdate).."")
+  group_welcome = string.gsub(group_welcome, '{rules}', ""..(rules or '').."")
+  group_welcome = string.gsub(group_welcome, '{about}', ""..(about or '').."")
+
+
+
+
+group_welcome = string.gsub(group_welcome, '{نام گروه}', msg.to.title)
+group_welcome = string.gsub(group_welcome, '{نام اول}', ""..(msg.action.user.first_name or '').."")
+ group_welcome = string.gsub(group_welcome, '{نام آخر}', ""..(msg.action.user.last_name or '').."")
+  group_welcome = string.gsub(group_welcome, '{نام کاربری}', "@"..(msg.action.user.username or '').."")
+  group_welcome = string.gsub(group_welcome, '{ساعت فارسی}', ""..(jdat.FAtime).."")
+  group_welcome = string.gsub(group_welcome, '{ساعت انگلیسی}', ""..(jdat.ENtime).."")
+  group_welcome = string.gsub(group_welcome, '{تاریخ فارسی}', ""..(jdat.FAdate).."")
+  group_welcome = string.gsub(group_welcome, '{تاریخ انگلیسی}', ""..(jdat.ENdate).."")
+
+ end
+return group_welcome
 end
 return {
   patterns = {
-  "^!!tgservice (chat_add_user)$",
-  "^!!tgservice (chat_add_user_link)$",
+  "^[!#/](setbye) +(.*)$",
+  "^[!#/](delbye)$",
+  "^!!tgservice (chat_del_user)$",
+  "^!!tgservice (channel_del_user)$",
   },
-  run = run,
-  pre_process = pre_process
+  run = run
 }
+end
