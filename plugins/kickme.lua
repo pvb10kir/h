4857613fcb8e30d02af55e8c2b2e562e42ae5540
3pehrdev <sepehrsalehi0111@gmail.com>
2016-07-16 17:20:53 +0430

    Update kickme.lua

--- plugins/kickme.lua
+++ plugins/kickme.lua
@@ -2,7 +2,7 @@
 if matches[1] == '/kickme' then
 local hash = 'kick:'..msg.to.id..':'..msg.from.id
      redis:set(hash, "waite")
-      local text = '*Hi* _"..msg.from.username.."_\n*Are you sure?*\n*Send Me* /yes *Or* /no'
+      local text = '*Hi*,\n*Are you sure?*\n*Send Me* /yes *Or* /no'
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')    
 end
 
@@ -11,7 +11,7 @@ end
       if msg.text:match("^/yes$") and redis:get(hash) == "waite" then
 	  redis:set(hash, "ok")
 	elseif msg.text:match("^/no$") and redis:get(hash) == "waite" then
-	send_api_msg(get_receiver_api(msg), "*Ok* "..msg.from.username..",\n *You're Free As a Bird*", true, 'md')
+	send_api_msg(get_receiver_api(msg), "*Ok*,\n *You're Free As a Bird*", true, 'md')
 	  redis:del(hash, true)
 
       end
@@ -20,7 +20,7 @@ end
 	 if redis:get(hash) then
         if redis:get(hash) == "ok" then
          channel_kick("channel#id"..msg.to.id, "user#id"..msg.from.id, ok_cb, false)
-         local text = '*User* ["..msg.from.print_name.."](https://telegram.me/hack_app_sec) *Succefulli Kicked*'
+         local text = '*User* *Succefulli Kicked*'
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')   
  end
       end
