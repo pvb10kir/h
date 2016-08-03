edaa753db777190b94fb728f846d71db1d38e297
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-04 00:37:05 +0430

    Update id2.lua

--- plugins/id2.lua
+++ plugins/id2.lua
@@ -1,36 +1,24 @@
-function run(msg,matches , result)
-    local user_id = msg.from.id
-    local chat_id = get_receiver(msg)
-    local token = "245112043:AAHX6B-4oBVBrRJ5z5_HAUGfGZx4EfYf_XE"
-
-    local db = 'https://api.telegram.org/bot'..token..'/getUserProfilePhotos?user_id='..user_id
-      local path = 'https://api.telegram.org/bot'..token..'/getFile?file_id='
-      local img = 'https://api.telegram.org/file/bot'..token..'/'
-    local res, code = https.request(db)
-      local jdat = json:decode(res)
-      local fileid = jdat.result.photos[1][3].file_id
-    local count = jdat.result.total_count
-    if tonumber(count) == 0 then
-      send_large_msg(chat_id,"Image Not Found",ok_cb,false)
-    else
-
-      local pt, code = https.request(path..fileid)
-        local jdat2 = json:decode(pt)
-        local path2 = jdat2.result.file_path
-    local link = img..path2
-    local photo = download_to_file(link,"ax"..user_id..".jpg")
-    send_photo2(chat_id, photo, "FullName:\n "..msg.from.first_name.."\n"
-.."User :\n "..msg.from.id.."\n"
-.."Username\n "..msg.from.username.."\n"
-.."User Group: \n"..msg.to.id.."\n"
-.."SuperGroup Name: \n"..msg.to.title.."\n"
-.." @SpheroCh", ok_cb, false)
-return 
+do
+function run(msg, matches)
+local reply_id = msg['id']
+local url = "http://8pic.ir/images/t84lc8c66illfknci8sz.png"
+local receiver = get_receiver(msg)
+local file = download_to_file(url)
+local info = '?? #Name: '..msg.from.first_name..'\n'
+..'?? #User : '..msg.from.id..'\n'
+..'?? #Username @'..msg.from.username..'\n'
+..'?? #User Group: '..msg.to.id..'\n'
+..'?? #Group Name: '..msg.to.title..'\n'
+..'?? #channel :  @thisismobin '
+if matches[1] == 'id' then
+send_photo2(receiver, file, info, rmtmp_cb, cb_extra)
 end
 end
 return {
 patterns = {
-"^[!/#]([Ii]d)$"
+    "^id"
 },
 run = run
 }
+
+end
