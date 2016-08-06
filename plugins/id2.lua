7ade6def70c4b3a1bacfa56bd2791bc15fdadbaa
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-07 02:14:59 +0430

    Create id2.lua

---
+++ plugins/id2.lua
@@ -0,0 +1,32 @@
+function run(msg,matches , result)
+    local user_id = msg.from.id
+    local chat_id = get_receiver(msg)
+    local token = "245112043:AAHX6B-4oBVBrRJ5z5_HAUGfGZx4EfYf_XE"
+    local db = 'https://api.telegram.org/bot'..token..'/getUserProfilePhotos?user_id='..user_id
+      local path = 'https://api.telegram.org/bot'..token..'/getFile?file_id='
+      local img = 'https://api.telegram.org/file/bot'..token..'/'
+    local res, code = https.request(db)
+      local jdat = json:decode(res)
+      local fileid = jdat.result.photos[1][3].file_id
+    local count = jdat.result.total_count
+    if tonumber(count) == 0 then
+      send_large_msg(chat_id,"Image Not Found",ok_cb,false)
+    else
+      local pt, code = https.request(path..fileid)
+        local jdat2 = json:decode(pt)
+        local path2 = jdat2.result.file_path
+    local link = img..path2
+    local photo = download_to_file(link,"ax"..user_id..".jpg")
+    send_photo2(chat_id, photo, "#YourName : "..msg.from.first_name.."\n"
+.."#Your ID :\n "..msg.from.id.."\n"
+.."#Group ID : \n"..msg.to.id.."\n"
+.."💋 @SpheroCh 💋", ok_cb, false)
+return 
+end
+end
+return {
+patterns = {
+"^[!/#]([Ii]d)$"
+},
+run = run
+}
