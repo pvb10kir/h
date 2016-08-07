0ff8eb307ca473f6a7cbf23b8cc216d5230ea389
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-07 12:38:56 +0430

    Update warn.lua

--- plugins/warn.lua
+++ plugins/warn.lua
@@ -79,7 +79,7 @@ local function warn_by_reply(extra, success, result) -- (on reply) /warn
   end
 ----------------------------------
   if is_momod2(msg.from.id, msg.to.id) and not is_admin2(extra.fromid) then
-  return send_msg(receiver, 'you can't warn to be group admin', ok_cb, false) end
+  return send_msg(receiver, 'you cant warn to be group admin', ok_cb, false) end
 --endif--
   if is_admin2(msg.from.id) then return send_msg(receiver, 'You can't warn admin group', ok_cb, false) end
 --endif--
