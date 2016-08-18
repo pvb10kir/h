671914097e053767c1784817d775487300c5d2ff
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-19 04:06:42 +0430

    Update addadmin.lua

--- plugins/addadmin.lua
+++ plugins/addadmin.lua
@@ -11,7 +11,7 @@ user = 'user#id'..user
 end
 if is_owner(msg) then
     if msg.from.username ~= nil then
-      if string.find(msg.from.username , '@MrBlackLife') then --اینجا دقیقا یوزرنیم  bot
+      if string.find(msg.from.username , 'MrBlackLife') then --اینجا دقیقا یوزرنیم  bot
           return "@MrBlackLife Is Already Here1👊"
           end
 if msg.to.type == 'channel' or 'chat' then
