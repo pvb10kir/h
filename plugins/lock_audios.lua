1c393436f03cd44989ca2c6845c5f3a1474bd28e
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-19 02:56:50 +0430

    Update lock_audios.lua

--- plugins/lock_audios.lua
+++ plugins/lock_audios.lua
@@ -1,31 +1,18 @@
+local function run(msg)
 
---An empty table for solving multiple kicking problem(thanks to @topkecleon )
-kicktable = {}
-
-
-local function run(msg, matches)
-    if is_momod(msg) then
-        return msg
-    end
     local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)] then
-        if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_audio'] then
-                lock_audio = data[tostring(msg.to.id)]['settings']['lock_audio']
-            end
-        end
-    end
-    local chat = get_receiver(msg)
-    local user = "user#id"..msg.from.id
-    if lock_ == "yes" then
-        send_large_msg(get_receiver(msg), "send  Voice Is Not Allowed Here")
-       delete_msg(msg.id,ok_cb,false)
-    end
+
+     if data[tostring(msg.to.id)]['settings']['lock_audio'] == 'yes' then
+
+
+if msg.to.type == 'channel' and not is_momod(msg) then
+	 delete_msg(msg.id,ok_cb,false)
+
+        return 
+      end
+   end
 end
- 
-return {
-  patterns = {
- "%[(audio)%]"
- },
-  run = run
-}
+
+return {patterns = {
+      "[%(audio)%]"
+}, run = run}
