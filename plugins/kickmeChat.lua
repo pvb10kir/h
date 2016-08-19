e08de450b3ff9ac8f2220efc787dab5338d54029
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-19 04:51:46 +0430

    Update kickmeChat.lua

--- plugins/kickmeChat.lua
+++ plugins/kickmeChat.lua
@@ -1,12 +1,15 @@
 local function run(msg)
-local text = [[*!kickme 📛
-kickme 📛
-/kickme 🎊*]]
+local text = [[_plugin kickme Patterns_
+_Only work by_ *[/]*]]
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
  patterns = {
 "^!kickme$",
 "^kickme$",
+"^!yes$",
+"^yes$",
+"^!no$",
+"^no$",
 },
  run = run }
