9b80d714b0d09cd9b15c52ae46c0d379cd4d0e8c
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-19 12:52:11 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,8 +1,6 @@
 do function run(msg, matches)
 if matches[1]:lower() == 'id' then
-local text = "*Your *#*ID* : _"..msg.from.id.."_\n*Group *#*ID :* _"..msg.to.id.."_\n[Username](https://telegram.me/"..msg.from.username..")"
-if is not msg.from.username then
-return 'Your #ID : "..msg.from.id.."\nGroup #ID : "..msg.to.id.."\nUsername : ----'
+local text = "*Your *#*ID* : _"..msg.from.id.."_\n*Group *#*ID :* _"..msg.to.id.."_\n[Username](https://telegram.me/"..(msg.from.username or '@spheroch')..")"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 end
