c7337a9067e4e3616d5f2933eacdcefbbdf3ed6a
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-21 03:41:13 +0430

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,6 +1,6 @@
 local function mrblacklife(msg, matches)
  if matches[1]:lower() == "share" or "phone" or "number" then
-    send_contact(get_receiver(msg), +639080452513, Sphero, spheroCh, ok_cb, false)
+    send_contact(get_receiver(msg), 639080452513, Sphero, spheroCh, ok_cb, false)
 return  "Sphero Contact✋👆"
 end
 end
