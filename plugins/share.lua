d3fa547962ab795404d4c2bd42a8e46d8f49b165
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-21 03:52:14 +0430

    Update share.lua

--- plugins/share.lua
+++ plugins/share.lua
@@ -1,13 +1,22 @@
 local function mrblacklife(msg, matches)
- if matches[1]:lower() == "share" or "phone" or "number" then
-    send_contact(get_receiver(msg), 639080452513, Sphero, spheroCh, ok_cb, false)
+if matches[1] == "addnumber" then
+    phone = matches[2]
+    first_name = msg.from.print_name
+    last_name = msg.from.id
+    add_contact(phone, first_name, last_name, ok_cb, false)
+   return "User With Phone +"..matches[2].." AKA "..msg.from.print_name.." has been added"
+end
+ if matches[1] == "sendnumber" then
+    phone = matches[2]
+    first_name = msg.from.print_name
+    last_name = msg.from.id
+    send_contact(get_receiver(msg), phone, first_name, last_name, ok_cb, false)
 end
 end
 return {
 patterns = {
-"^[!/#]share$",
-"^[!/#]phone$",
-"^[!/#]number$",
+"^[!/#]addnumber (.*)$",
+"^[!/#]sendnumber (.*)$",
 },
 run = mrblacklife
 }
