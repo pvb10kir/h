9bb406855c3baa3cd457cb81a96aacbf713e3140
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-24 00:08:36 +0430

    Update expire.lua

--- plugins/expire.lua
+++ plugins/expire.lua
@@ -1,71 +1,72 @@
+
 local function pre_process(msg)
-	local timetoexpire = 'unknown'
-	local expiretime = redis:hget ('expiretime', get_receiver(msg))
-	local now = tonumber(os.time())
-	if expiretime then    
-		timetoexpire = math.floor((tonumber(expiretime) - tonumber(now)) / 86400) + 1
-		if tonumber("0") > tonumber(timetoexpire) and not is_admin(msg) then
-		if msg.text:match('/') then
-			return send_large_msg(get_receiver(msg), ' تاریخ اتقضای گروه به پایان رسید و ازین به بعد ربات در گروه شما کار نخواهد کرد.')
-		else
-			return
-		end
-	end
-	if tonumber(timetoexpire) == 0 then
-		if redis:hget('expires0',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '0 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\n تمدید در @Sphero_Bot')
-		redis:hset('expires0',msg.to.id,'5')
-	end
-	if tonumber(timetoexpire) == 1 then
-		if redis:hget('expires1',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '1 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\n تمدید در @Sphero_Bot')
-		redis:hset('expires1',msg.to.id,'5')
-	end
-	if tonumber(timetoexpire) == 2 then
-		if redis:hget('expires2',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '2 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\n تمدید در @Sphero_Bot')
-		redis:hset('expires2',msg.to.id,'5')
-	end
-	if tonumber(timetoexpire) == 3 then
-		if redis:hget('expires3',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '3 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\n تمدید در @Sphero_Bot')
-		redis:hset('expires3',msg.to.id,'5')
-	end
-	if tonumber(timetoexpire) == 4 then
-		if redis:hget('expires4',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '4 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\n تمدید در @Sphero_Bot')
-		redis:hset('expires4',msg.to.id,'5')
-	end
-	if tonumber(timetoexpire) == 5 then
-		if redis:hget('expires5',msg.to.id) then return msg end
-		send_large_msg(get_receiver(msg), '5 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\n تمدید در @Sphero_Bot')
-		redis:hset('expires5',msg.to.id,'5')
-	end
+  local timetoexpire = 'unknown'
+  local expiretime = redis:hget ('expiretime', get_receiver(msg))
+  local now = tonumber(os.time())
+  if expiretime then    
+    timetoexpire = math.floor((tonumber(expiretime) - tonumber(now)) / 86400) + 1
+    if tonumber("0") > tonumber(timetoexpire) and not is_sudo(msg) then
+    if msg.text:match('/') then
+      return send_large_msg(get_receiver(msg), 'تاریخ اتقضای گروه به پایان رسید.\n ربات دیگر در گروه شما کار نخواهد کرد\nتمدید در @Sphero_Bot')
+    else
+      return
+    end
+  end
+  if tonumber(timetoexpire) == 0 then
+    if redis:hget('expires0',msg.to.id) then return msg end
+    send_large_msg(get_receiver(msg), '0 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\nنسبت به تمدید اقدام کنید.\nتمدید با مراجعه به @Sphero_Bot\n!!بخش امور ربات اسفرو/تمدید گپ')
+    redis:hset('expires0',msg.to.id,'5')
+  end
+  if tonumber(timetoexpire) == 1 then
+    if redis:hget('expires1',msg.to.id) then return msg end
+    send_large_msg(get_receiver(msg), '1 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\nنسبت به تمدید اقدام کنید.\nتمدید با مراجعه به @Sphero_Bot\n!!بخش امور ربات اسفرو/تمدید گپ')
+    redis:hset('expires1',msg.to.id,'5')
+  end
+  if tonumber(timetoexpire) == 2 then
+    if redis:hget('expires2',msg.to.id) then return msg end
+    send_large_msg(get_receiver(msg), '2 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\nنسبت به تمدید اقدام کنید.\nتمدید با مراجعه به @Sphero_Bot\n!!بخش امور ربات اسفرو/تمدید گپ')
+    redis:hset('expires2',msg.to.id,'5')
+  end
+  if tonumber(timetoexpire) == 3 then
+    if redis:hget('expires3',msg.to.id) then return msg end
+    send_large_msg(get_receiver(msg), '3 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\nنسبت به تمدید اقدام کنید.\nتمدید با مراجعه به @Sphero_Bot\n!!بخش امور ربات اسفرو/تمدید گپ')
+    redis:hset('expires3',msg.to.id,'5')
+  end
+  if tonumber(timetoexpire) == 4 then
+    if redis:hget('expires4',msg.to.id) then return msg end
+    send_large_msg(get_receiver(msg), '4 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\nنسبت به تمدید اقدام کنید.\nتمدید با مراجعه به @Sphero_Bot\n!!بخش امور ربات اسفرو/تمدید گپ')
+    redis:hset('expires4',msg.to.id,'5')
+  end
+  if tonumber(timetoexpire) == 5 then
+    if redis:hget('expires5',msg.to.id) then return msg end
+    send_large_msg(get_receiver(msg), '5 روز تا پایان تاریخ انقضای گروه باقی مانده است\nنسبت به تمدید اقدام کنید.\nتمدید با مراجعه به @Sphero_Bot\n!!بخش امور ربات اسفرو/تمدید گپ!!')
+    redis:hset('expires5',msg.to.id,'5')
+  end
 end
 return msg
 end
 function run(msg, matches)
-	if matches[1]:lower() == 'setexpire' then
-		if not is_admin(msg) then return end
-		local time = os.time()
-		local buytime = tonumber(os.time())
-		local timeexpire = tonumber(buytime) + (tonumber(matches[2]) * 86400)
-		redis:hset('expiretime',get_receiver(msg),timeexpire)
-		return "👊Group Expire Seted For "..matches[2].." Day"
-	end
-	if matches[1]:lower() == 'expire' then
-		local expiretime = redis:hget ('expiretime', get_receiver(msg))
-		if not expiretime then return 'No Expire seted For this Group' else
-			local now = tonumber(os.time())
-			return (math.floor((tonumber(expiretime) - tonumber(now)) / 86400) + 1) .. " Day"
-		end
-	end
+  if matches[1]:lower() == 'setexpire' then
+    if not is_admin2(msg) then return end
+    local time = os.time()
+    local buytime = tonumber(os.time())
+    local timeexpire = tonumber(buytime) + (tonumber(matches[2]) * 86400)
+    redis:hset('expiretime',get_receiver(msg),timeexpire)
+    return "I Set Group Expire For"..matches[2].. " Day📍."
+  end
+  if matches[1]:lower() == 'expire' then
+    local expiretime = redis:hget ('expiretime', get_receiver(msg))
+    if not expiretime then return 'تاریخ ست نشده است' else
+      local now = tonumber(os.time())
+      return (math.floor((tonumber(expiretime) - tonumber(now)) / 86400) + 1) .. " روز دیگر"
+    end
+  end
 
 end
 return {
   patterns = {
-    "^[!/](setexpire) (.*)$",
-	"^[!/]([Ee]xpire)$",
+    "^[!/]([Ss]etexpire) (.*)$",
+  "^[!/]([Ee]xpire)$",
   },
   run = run,
   pre_process = pre_process
