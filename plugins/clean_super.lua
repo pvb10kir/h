cd4b0dad70e906d6791bee45f22f04419be41bc0
3pehrdev <sepehrsalehi0111@gmail.com>
2016-08-24 21:14:30 +0430

    Update clean_super.lua

--- plugins/clean_super.lua
+++ plugins/clean_super.lua
@@ -1,3 +1,5 @@
+
+
 local function check_member_super_deleted(cb_extra, success, result)
 local receiver = cb_extra.receiver
  local msg = cb_extra.msg
@@ -6,12 +8,12 @@ if success == 0 then
 send_large_msg(receiver, "im not admin") 
 end
 for k,v in pairs(result) do
-  if v.firstname then
+  if v.first_name then
 deleted = deleted + 1
  kick_user(v.peer_id,msg.to.id)
  end
  end
- send_large_msg(receiver, deleted.." There Are 📍☜Members!") 
+ send_large_msg(receiver, deleted.." members Cleanded!") 
  end 
 local function run(msg, matches)
 if is_owner(msg) then
@@ -24,4 +26,3 @@ channel_get_users(receiver, check_member_super_deleted,{receiver = receiver, msg
     "^[/#!](clean member)$"
  },
 run = run,
-}
