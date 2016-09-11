44cbcc83c4fb58379718323489b4b95acc7c53e2
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-11 18:08:02 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -4,6 +4,7 @@ local hash = 'group:'..msg.to.id
     local group_lang = redis:hget(hash,'lang')
     if group_lang then
 local text = "["..msg.from.print_name.."](https://Telegram.Me/"..(msg.from.username or 'sphero_ch')..")\n*lایدی شماl* : _"..msg.from.id.."_\n*lایدی گروهl :* _"..msg.to.id.."_"
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 else
 local text = "["..msg.from.print_name.."](https://Telegram.Me/"..(msg.from.username or 'sphero_ch')..")\n*Your *#*ID* : _"..msg.from.id.."_\n*Group *#*ID :* _"..msg.to.id.."_"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
