a40958a294c2e32f4aaf7e9c2d0ddc693a606f12
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-11 18:05:11 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,5 +1,10 @@
 do function run(msg, matches)
 if matches[1]:lower() == 'id' then
+local hash = 'group:'..msg.to.id
+    local group_lang = redis:hget(hash,'lang')
+    if group_lang then
+local text = "["..msg.from.print_name.."](https://Telegram.Me/"..(msg.from.username or 'sphero_ch')..")\n*lایدی شماl* : _"..msg.from.id.."_\n*lایدی گروهl :* _"..msg.to.id.."_"
+else
 local text = "["..msg.from.print_name.."](https://Telegram.Me/"..(msg.from.username or 'sphero_ch')..")\n*Your *#*ID* : _"..msg.from.id.."_\n*Group *#*ID :* _"..msg.to.id.."_"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
@@ -12,7 +17,7 @@ moderator = {
 },
 patterns = {
 "^[!/#]([Ii][Dd])$",
-"^[Ii][Dd]$",
+-- "^[Ii][Dd]$", --For lock cmds
 },
 run = run
 }
