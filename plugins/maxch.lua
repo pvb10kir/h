8f9c37e050ee43b61a5287355d3dc25626082640
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-14 22:23:14 +0430

    Update maxch.lua

--- plugins/maxch.lua
+++ plugins/maxch.lua
@@ -1,7 +1,7 @@
 local function mrblacklife(msg, matches)
-  if matches[1] == 'maxchar' and is_momod(msg) then
+  if matches[1] == 'maxch' and is_momod(msg) then
 redis:set('max_char'..msg.to.id,matches[2])
-    return 'maxchar has been set to '..matches[2]..' chatracters.'
+    return 'maxch has been set to '..matches[2]..' chatracters.'
     end
 end
 local function pre_process(msg)
@@ -9,10 +9,12 @@ if string.len(matches[1]) > redis:get('max_char'..msg.to.id) and not is_momod(ms
 delete_msg(msg.id,ok_cb,false)
 return 'More than '..redis:get('max_char'..msg.to.id)..' characters are not allowed'
   end
+return msg
+end
 end
     return {
   patterns = {
-  "^[!/#](maxchar) (.*)$",
+  "^[!/#](maxch) (.*)$",
   "(.*)"
   },
   run = mrblacklife,
