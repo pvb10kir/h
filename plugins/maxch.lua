6816f5cfcf8ea0cd1017da31f77bb9dd1f90b592
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-14 21:11:03 +0430

    Update maxch.lua

--- plugins/maxch.lua
+++ plugins/maxch.lua
@@ -1,10 +1,11 @@
 local function mrblacklife(msg, matches)
 	if 'maxch' and matches[3] and is_momod(msg) then
+		return 'maxch has been set to '..matches[3]..' chatracters.'
+		end
 		text = matches[2]
 		if string.len(text) > matches[3] then
 	delete_msg(msg.id,ok_cb,false)
 return 'More than '..matches[3]..' characters are not allowed'
-end
 	end
 		end
 		return {
