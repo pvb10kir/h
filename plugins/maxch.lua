bce92dbab27749a9535b3f8e9896ee94d7c92705
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-14 21:01:29 +0430

    Create maxch.lua

---
+++ plugins/maxch.lua
@@ -0,0 +1,14 @@
+local function mrblacklife(msg, matches)
+	if 'maxch' and matches[3] and is_momod(msg) then
+		text = matches[2]
+		if string.len(text) > matches[3] then
+	delete_msg(msg.id,ok_cb,false)
+return 'More than '..matches[3]..' characters are not allowed'
+	end
+		end
+		return {
+	patterns = {
+	"^[!/#]maxch (.*)$",
+	},
+	run = mrblacklife,
+	}
