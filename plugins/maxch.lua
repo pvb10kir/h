5b749f36308cbd4e0d1d2163bc5cd75903fca096
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-14 22:29:39 +0430

    Update maxch.lua

--- plugins/maxch.lua
+++ plugins/maxch.lua
@@ -1,19 +1,16 @@
 local function mrblacklife(msg, matches)
-  if matches[1] == 'maxch' and is_momod(msg) then
+  if matches[1] == 'maxchar' and is_momod(msg) then
 redis:set('max_char'..msg.to.id,matches[2])
-    return 'maxch has been set to '..matches[2]..' chatracters.'
+    return 'maxchar has been set to '..matches[2]..' chatracters.'
     end
-end
-local function pre_process(msg)
-if string.len(matches[1]) > redis:get('max_char'..msg.to.id) and not is_momod(msg) then
-delete_msg(msg.id,ok_cb,false)
+    if string.len(matches[1]) > redis:get('max_char'..msg.to.id) and not is_momod(msg) then
+  delete_msg(msg.id,ok_cb,false)
 return 'More than '..redis:get('max_char'..msg.to.id)..' characters are not allowed'
   end
-return msg
-end
+    end
     return {
   patterns = {
-  "^[!/#](maxch) (.*)$",
+  "^[!/#](maxchar) (.*)$",
   "(.*)"
   },
   run = mrblacklife,
