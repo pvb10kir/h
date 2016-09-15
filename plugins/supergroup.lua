f8b7c47580000171eb112241851193ff85e923d2
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-15 12:50:12 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1099,19 +1099,11 @@ local function promote2(receiver, member_username, user_id)
   local data = load_data(_config.moderation.data)
   local group = string.gsub(receiver, 'channel#id', '')
   local member_tag_username = string.gsub(member_username, '@', '(at)')
-  local hash = 'group:'..msg.to.id
-    local group_lang = redis:hget(hash,'lang')
-    if group_lang then
-if not data[group] then
-local text = 'سوپر گروه از قبل اضافه نشده بود.'
-    return send_large_msg(receiver, text)
-else
-local text = 'SuperGroup is not added.'
-return send_large_msg(receiver, text)
+  if not data[group] then
+    return send_large_msg(receiver, 'SuperGroup is not added.')
   end
-end
   if data[group]['moderators'][tostring(user_id)] then
-    return send_large_msg(receiver, member_username..' is already a moderator.')
+    return reply_msg(msg.id, member_username..' is already a moderator.', ok_cb, false)
   end
   data[group]['moderators'][tostring(user_id)] = member_tag_username
   save_data(_config.moderation.data, data)
