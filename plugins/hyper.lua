4150b217556cec2602457bb2410650932abb0061
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-17 01:25:40 +0430

    Update hyper.lua

--- plugins/hyper.lua
+++ plugins/hyper.lua
@@ -1,5 +1,8 @@
 local function run(msg,matches)
 local text = "*Your Text* : \n"..matches[1]
+else
+matches[1] = */kickme* 
+return 'Khar khodeti'
   send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
