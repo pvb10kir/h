e7a1c68337c4ad24e5d7282133eaa4999a80f8a6
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-21 01:35:43 +0330

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -1,7 +1,176 @@
 local function run(msg)
-local text = [[*Local Help For* [Sphero](http://telegram.me/sphero)
+local hash = 'group:'..msg.to.id
+    local group_lang = redis:hget(hash,'lang')
+    if group_lang then
+local text = [[*راهنمای جهانی برای رباتِ* [Sphero](http://telegram.me/sphero)
+*⌚️!gpinfo
+نشون دادن اطلاعات گروه
+⌚️!admins
+نشون دادن ادمین های گروه
+!owner
+نمایش اونر گروه
+⌚️!modlist
+نمایش لیست مدیر ها
+⌚️!bots
+نمایش دادن بات هایی که در گروه قرار دارند
+⌚️!who
+لیست کسانی ک در گروه قرار دارند به صورت فایل
+⌚️!block
+گذاشتن یک نفر در لیست سیاه یا بلاک لیست گروه
+⌚️!ban
+بن کردن یک نفر
+⌚️!unban
+برداشتن بن از یک نفر
+⌚️!id
+دریافت ایدی خود و سوپرگروه
+⌚️!id from
+دریافت ایدی با ریپلی روی پیام فروارد شده
+⌚️/kickme / /yes / /no
+با زدن دستور /kickme و سپس با زدن دستور /yes از گروه خارج شوید
+⌚️!setowner
+اونر کردن یک فرد در گروه
+⌚️ !promote username|id
+مدیر کردن یک نفر
+⌚️!demote username|id
+گرفتن مقام مدیر
+⌚️!setname text
+عوض کردن اسم گروه
+⌚️!setphoto
+عوض کردن عکس گروه
+⌚️!setrules Text
+گذاشتن قوانین برای گروهتون
+⌚️!setabout text
+گذاشتن اطلاعات بیشتر برای گروهتون
+⌚️!save value <text>
+سیو کردن ی متن برای گروه
+⌚️!get value
+دریافت متن سیو شده
+⌚️!newlink
+ساخت لینک جدید برای گروه (فقط گروه هایی که با ربات ساخته شده اند
+⌚️!link
+دریافت لینک گروه
+⌚️!rules
+نمایش قوانین گروه
+⌚️!lock links|flood|spam|Arabic|member|rtl|sticker|contacts|strict or... settings text
+قفل کردن یک چیز مجاز
+⌚️!unlock links|flood|spam|Arabic|member|sticker|contacts|strict or.. settings text
+ازاد کردن یک قفل
+⌚️!mute all|audio|gifs|photo|video|service
+موت کردن یک فرمت لیست 👆
+⌚️!unmute all|audio|gifs|photo|video|service
+انموت کردن یک فرمت لیست👆
+⌚️!setflood value
+تعیین کردن این که با چند عدد اسپم فرد از گروه اخراج شود
+⌚️!settings
+نمایش تنظیمات گروه
+⌚️!muteslist
+نمایش لیست فرمت های موت شده یا مجاز
+⌚️!muteuser username
+با این دستور  میتونید اجازه چت کردن از فرد خاصی را بگیرید و با دوباره زدن این دستور و ریپلی کردن نام شخص میتونید اجازه چت کردن رو ب اون برگردونید
+⌚️!mutelist
+نمایش لیست کسانی که موت شده اند
+⌚️!banlist
+نمایش لیست بن گروه
+!clean rules|about|modlist|mutelist
+⌚️!del
+پاک کردن یک پیام با ریپلی
+⌚️!public yes|no
+عمومی کردن یا خصوصی کردن گروه
+⌚️!res username
+دریافت ایدی و یوزرنیم شخص
+⌚️!log
+نشان دادن سیستم گروه
 
-*⌚️!info
+🇮🇷!addsudo 
+دعوت کردن ادمین ربات به گروه شما
+
+🇮🇷!calc (معادله)
+مثال calc 2+2!
+
+🇮🇷!date 
+نمایش تاریخ قمری
+
+🇮🇷!dogify (متن)
+نوشتن متن روی عکس سگ
+
+🇮🇷!filter (+ یا -) [word - کلمه]
+ممنوع کردن لغت در گروه
+
+🇮🇷github (username/project)
+جستجوی گیتهاب مثال : github 3pehrdev/sphero
+
+🇮🇷info
+نمایش اطلاعات شما
+
+🇮🇷/keep calm (text1) (text2) (text3)
+نوشتن متن مورد نظر روی لوگوی keepcalm مثال :
+/keep calm and best friend
+
+🇮🇷!me
+نمایش تعداد پیام های ارسالی + درصد
+
+🇮🇷!music (نام خاننده)
+وقتی که دستور بالا رو بزنید ی لیست میاره که میتونید با دستور 
+/dl (شماره کنار اهنگ)
+لینک دانلود رو دریافت کنید
+
+🇮🇷!rmsg (تعداد)
+پاک کردن پیام ها به تعداد دلخواه
+
+🇮🇷!gif (اسم)
+ساخت گیف با متن دلخواه
+
+🇮🇷!support
+دریافت لینک گروه پشتیبانی
+
+🇮🇷!ver
+نمایش ورژن ربات
+
+🇮🇷!warn (username/reply)
+اخطار به کاربر در صورت رعایت نکردن قوانین
+
+🇮🇷!unwarn (username/reply)
+پاک کردن ۱ اخطار کاربر 
+
+🇮🇷!unwarnall (username/reply)
+پاک کردن کل اخطار های کاربر
+
+🇮🇷!warnmax (1-10)
+تنظیم کردن که در چه تعداد اخطار کاربر از گروه پاک شود
+
+🇮🇷!getwarn (username/reply)
+نمایش تعداد اخطار های کاربر
+
+🇮🇷!photo
+روی استیکر مورد نظر ریپلی کنید و دستور بالارو بزنید تا ربات اونو به عکس تبدیل کنه
+
+🇮🇷!sticker
+روی عکس مورد نظر ریپلی کنید و دستور بالارو بزنید تا ربات اونو به عکس تبدیل کنه
+
+🇮🇷!expire
+نمایش زمان باقی مانده از تاریخ انقضای گروه خود
+
+🇮🇷!clean member
+پاک کردن تمام ممبر های سوپر گروه
+
+🇮🇷!setlang (fa/en)
+تنظیم کردن زبان ربات*
+
+🇮🇷!feedback پیام
+فرستادن پیام شما برای ادمین ربات
+
+🇮🇷!tagall پیام
+نمایش یوزرنیم های تمام کاربران گروه
+
+🇮🇷!type موضوع
+ست کردن موضوعی برای گروه خود که در دریافت لینک از ان استفاده میشود همچنین در متن تنظیمات
+
+[💤SpheroTM💤
+Channel](https://telegram.me/sphero_ch)]]
+else
+local text = [[*Local Help For*[ Sphero](https://telegram.me/sphero_ch)
+*
+⌚️!info
 Displays general info about the SuperGroup
 
 ⌚️!admins
@@ -128,19 +297,99 @@ Returns users name and id by username
 
 ⌚️!log
 Returns group logs
-⌚️Search for kick reasons using #RTL|#spam|#lockmember*
+⌚️Search for kick reasons using #RTL|#spam|#lockmember
 
-_👌Thx For See This👌
-More Cmds Send me_ [/more_help](https://telegram.me/sphero)
-برای دیدن دستورات بیشتر
-/more help 
-را ارسال کنید
 
-*SpheroTM*
+🇮🇷!addsudo 
+addsudo to your group
 
-[برای ادد کردن رایگان بات به سوپر گروه هاتون روی همین متن زده و
-از طریق دکمه (پیام رسان - Private) لینک گروه خود را برای ما ارسال کنید تا در اسرع وقت ربات رو ب گروه شما بیاریم](http://telegram.me/sphero_bot)]]
+🇮🇷!calc (معادله)
+ex: calc 2+2!
+
+🇮🇷!date 
+show ghamari date
+
+🇮🇷!dogify (text)
+write your text of dogify logo
+
+🇮🇷!filter (+ یا -) [word - کلمه]
+Filter or unfilter words
+
+🇮🇷github (username/project)
+github search. ex: : github 3pehrdev/sphero
+
+🇮🇷info
+return your info
+
+🇮🇷/keep calm (text1) (text2) (text3)
+Just write your text of keep calm logo. ex:
+/keep calm and best friend
+
+🇮🇷!me
+Your messages info
+
+🇮🇷!music (نام خاننده)
+u can use 👆 and👇
+/dl (Number)
+For recieve download link
+
+🇮🇷!rmsg (number)
+Remove group messages 
+
+🇮🇷!gif (name)
+Create gif by your name
+
+🇮🇷!support
+Recieve support group link
+
+🇮🇷!ver
+Show bot version
+
+🇮🇷!warn (username/reply)
+warn the users 
+
+🇮🇷!unwarn (username/reply)
+remove 1 warns
+
+🇮🇷!unwarnall (username/reply)
+Remove all warns of user
+
+🇮🇷!warnmax (1-10)
+first ser max number of warns 
+
+🇮🇷!getwarn (username/reply)
+Show user warns
+
+🇮🇷!photo
+Just reply your sticker
+
+🇮🇷!sticker
+Just reply your Photo
+
+🇮🇷!expire
+Show Expire Time
+
+🇮🇷 !clean member
+Remove All Member Of Supergroup
+
+🇮🇷 !setlang (fa/en)
+Set your group language
+
+🇮🇷 !feedback msg
+Send your feedback to my sudo
+
+🇮🇷!tagall text
+show all members username
+
+🇮🇷!type subject
+Set subject for your group 
+subject was show to your group link 
+want to test? Send me /link
+*
+*💤SpheroTM💤*
+     [Channel](https://telegram.me/sphero_ch)]]
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+end
 end
 return {
  patterns = {"^[/#!]help$",},
