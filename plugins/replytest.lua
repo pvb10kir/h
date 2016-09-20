980c01257a2b0af10f29883d0e72345c53beeafc
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-21 02:08:47 +0330

    Update replytest.lua

--- plugins/replytest.lua
+++ plugins/replytest.lua
@@ -2,7 +2,6 @@ local function run(msg, matches)
 local text = 'تست کار دادن ریپلی دو زبانه با موفقیت انجام شد👊\nپلاگین سالم است
 reply_msg(msg.id, text, ok_cb, false)
 end
-end
 return {
 patterns = {
 "[!/]reply test$",
