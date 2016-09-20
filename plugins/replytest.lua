98d5f2bb4b345c7aeb2ecd100327aa5ce1676001
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-21 02:13:06 +0330

    Update replytest.lua

--- plugins/replytest.lua
+++ plugins/replytest.lua
@@ -1,6 +1,13 @@
 local function run(msg, matches)
+local hash = 'group:'..msg.to.id
+    local group_lang = redis:hget(hash,'lang')
+    if group_lang then
 local text = 'تست کار دادن ریپلی دو زبانه با موفقیت انجام شد👊\nپلاگین سالم است'
 reply_msg(msg.id, text, ok_cb, false)
+else
+local text = 'Reply 2 lang test done successfully\nplugin fixed'
+reply_msg(msg.id, text, ok_cb, false)
+end
 end
 return {
 patterns = {
