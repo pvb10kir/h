957ed8f0200adfb3987bcde14270d1b7b7faa1b8
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-23 21:41:48 +0330

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -2453,7 +2453,7 @@ send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 				local receiver = get_receiver(msg)
 				local get_cmd = "mute_user"
 				muteuser = get_message(msg.reply_id, get_message_callback, {receiver = receiver, get_cmd = get_cmd, msg = msg})
-			elseif matches[1] == "unmuteuser" and string.match(matches[2], '^%d+$') then
+			elseif matches[1] == "muteuser" and string.match(matches[2], '^%d+$') then
 				local user_id = matches[2]
 				if is_muted_user(chat_id, user_id) then
 					unmute_user(chat_id, user_id)
@@ -2463,6 +2463,21 @@ send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 					mute_user(chat_id, user_id)
 					savelog(msg.to.id, name_log.." ["..msg.from.id.."] added ["..user_id.."] to the muted users list")
 					return "["..user_id.."] added to the muted user list"
+				end
+					if matches[1] == "unmuteuser" and is_momod(msg) then
+			local chat_id = msg.to.id
+			local hash = "unmute_user"..chat_id
+			local user_id = ""
+			if type(msg.reply_id) ~= "nil" then
+				local receiver = get_receiver(msg)
+				local get_cmd = "unmute_user"
+				muteuser = get_message(msg.reply_id, get_message_callback, {receiver = receiver, get_cmd = get_cmd, msg = msg})
+			elseif matches[1] == "unmuteuser" and string.match(matches[2], '^%d+$') then
+				local user_id = matches[2]
+				if is_muted_user(chat_id, user_id) then
+					unmute_user(chat_id, user_id)
+					savelog(msg.to.id, name_log.." ["..msg.from.id.."] removed ["..user_id.."] from the muted users list")
+					return "["..user_id.."] removed from the muted users list"
 				end
 			elseif matches[1] == "muteuser" and not string.match(matches[2], '^%d+$') then
 				local receiver = get_receiver(msg)
