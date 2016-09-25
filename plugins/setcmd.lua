7a9d53a1166252ead7dce44a005af9903691efba
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-25 23:24:47 +0330

    Update setcmd.lua

--- plugins/setcmd.lua
+++ plugins/setcmd.lua
@@ -3,13 +3,7 @@ if matches[1]:lower() == 'setcmd' and matches[2] then
         if is_owner(msg) then
             hash = 'cmdset:'..msg.to.id
             redis:set(hash, matches[2])
-local hash2 = 'group:'..msg.to.id
-    local group_lang = redis:hget(hash2,'lang')
-    if group_lang then
              return 'دستورات گروه تنظیم شد روی '..matches[2]..' .'
-else
-    return 'done cmds set to '..matches[2]..' .'
-        end
   end
 end
         end
