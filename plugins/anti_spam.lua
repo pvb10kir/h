af500705221eb656cdcab148e608f0cc7683380c
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-25 23:18:32 +0330

    Update anti_spam.lua

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -135,8 +135,8 @@ send_api_msg(msg, get_receiver_api(msg), "*کاربر [*_"..msg.from.id.."_*]*\n
           local print_name = user_print_name(msg.from):gsub("‮", "")
 		  local name = print_name:gsub("_", "")
           --Send this to that chat
-           send_large_msg("chat#id"..msg.from.id, "User [ "..name.." ]"..msg.from.id.." globally banned from :\n["..msg.to.id.."] aka ("..msg.to.title..") For Spamming\n u Can unbanall User by\n/unbanall_"..msg.from.id..")
-		  send_large_msg("channel#id"..msg.from.id, "User [ "..name.." ]"..msg.from.id.." globally banned from :\n["..msg.to.id.."] aka ("..msg.to.title..") For Spamming\n u Can unbanall User by\n/unbanall_"..msg.from.id..")
+                 send_large_msg("chat#id"..msg.to.id, "User [ "..name.." ]"..msg.from.id.." globally banned (spamming)")
+send_large_msg("channel#id"..msg.to.id, "User [ "..name.." ]"..msg.from.id.." globally banned (spamming)")
           local GBan_log = 'GBan_log'
 		  local GBan_log =  data[tostring(GBan_log)]
 		  for k,v in pairs(GBan_log) do
