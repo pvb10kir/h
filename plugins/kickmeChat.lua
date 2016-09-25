0c1c1a83ca6bb97ebc394f3b05c899c6691e8d87
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-25 19:20:46 +0330

    Update kickmeChat.lua

--- plugins/kickmeChat.lua
+++ plugins/kickmeChat.lua
@@ -1,8 +1,15 @@
 local function run(msg)
+    local hash = 'group:'..msg.to.id
+    local group_lang = redis:hget(hash,'lang')
+    if group_lang then
+        local text = 'کار میکنند (/) دستورات این پلاگین فقط با '
+            send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+        else
 local text = [[_plugin kickme Patterns_
 _Only work by_ *[/]*]]
     send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
+    end
 return {
  patterns = {
 "^!kickme$",
