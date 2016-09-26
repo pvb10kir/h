d34012adc652a338dbadbe35992f3cac6cc677f6
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-26 18:27:48 +0330

    Update setcmd.lua

--- plugins/setcmd.lua
+++ plugins/setcmd.lua
@@ -2,7 +2,7 @@ local function run(msg, matches, callback, extra)
 if matches[1]:lower() == 'setcmd' and matches[2];lower() == 'fa' then
         if is_owner(msg) then
             hash = 'cmdset:'..msg.to.id
-            redis:set(hash, fa)
+            redis:hset(hash, fa)
              return 'دستورات گروه تنظیم شد روی فارسی .'
   end
 end
