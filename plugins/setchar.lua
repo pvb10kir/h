7b10dced6e8e44c89ee532b861a428677e7aac69
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-29 00:31:45 +0330

    Update and rename maxch.lua to setchar.lua

--- plugins/setchar.lua
+++ plugins/setchar.lua
@@ -1,7 +1,7 @@
 local function mrblacklife(msg, matches)
-  if matches[1] == 'maxch' and is_momod(msg) then
+  if matches[1] == 'setchar' and is_momod(msg) then
 redis:set('max_char'..msg.to.id,matches[2])
-    return 'maxch has been set to '..matches[2]..' chatracters.'
+    return 'maxchar has been set to '..matches[2]..' chatracters.'
     end
     if string.len(matches[1]) > redis:get('max_char'..msg.to.id) and not is_momod(msg) then
   delete_msg(msg.id,ok_cb,false)
@@ -10,7 +10,7 @@ return 'More than '..redis:get('max_char'..msg.to.id)..' characters are not allo
     end
     return {
   patterns = {
-  "^[!/#](maxch) (.*)$",
+  "^[!/#](setchar) (.*)$",
   "(.*)"
   },
   run = mrblacklife,
