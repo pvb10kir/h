44952d0610cc779392bdc8c0b3c7a9b8c72c00e9
3pehrdev <sepehrsalehi0111@gmail.com>
2016-09-29 02:06:23 +0330

    Update bchyper.lua

--- plugins/bchyper.lua
+++ plugins/bchyper.lua
@@ -1,14 +1,13 @@
    function run(msg, matches)
 if matches[1]:lower() == 'bchyper' and matches[2] and is_sudo(msg) then
-local pm = matches[2]
     local data = load_data(_config.moderation.data)
 local groups = 'groups'
       for k,v in pairs(data[tostring(groups)]) do
         chat_id =  v
         local chat = 'chat#id'..chat_id
         local channel = 'channel#id'..chat_id
-      send_api_msg(msg, get_receiver_api(msg), pm, true, 'md')
-send_api_msg(msg, get_receiver_api(msg), pm, true, 'md')
+      send_api_msg(msg, get_receiver_api(msg), matches[2], true, 'md')
+send_api_msg(msg, get_receiver_api(msg), matches[2], true, 'md')
 end
   end
       end 
