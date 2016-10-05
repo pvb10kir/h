750597a7f468998f8bcd87ad995cd49ba7ca442c
3pehrdev <sepehrsalehi0111@gmail.com>
2016-10-05 20:58:24 +0330

    Create leave.lua

---
+++ plugins/leave.lua
@@ -0,0 +1,18 @@
+local function run(msg, matches)
+if not is_sudo(msg) then
+return false
+end
+local bot_id = 196009168 --for example 271787100
+if matches[1] == "leave" then
+chat_del_user("chat#id"..matches[2], 'user#id'..bot_id, ok_cb, false)
+leave_channel("channel#id"..matches[2], ok_cb, false)
+send_large_msg(channel#id'..matches[2], 'ربات به دستور سودو از گروه خارج شد \nمراجعه کنید @MrBlackLife برای اطلاعات بیشتر به\n@Sphero_Ch' )
+return "Success! Bot leave from "..matches[2]
+end
+end
+return {
+  patterns = {
+     "^[!#/](leave)_(.*)$"
+  },
+     run = run
+}
