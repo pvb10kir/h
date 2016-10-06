036864e77f9290a8a5989b0799e40111b3673876
3pehrdev <sepehrsalehi0111@gmail.com>
2016-10-06 11:14:08 +0330

    Update join.lua

--- plugins/join.lua
+++ plugins/join.lua
@@ -4,7 +4,7 @@ local group = matches[2]
 local user = msg.from.id
 
     if matches[1]:lower() == "join" then
-       channel_invite_user("channel#id"..support, 'user#id'..user, ok_cb, false)
+       channel_invite_user("channel#id"..group, 'user#id'..user, ok_cb, false)
        send_large_msg("channel#id"..group, 'Hi @'..msg.from.username.. ' \nWelcome To '..group..'\n@Sphero_Ch')
        return "You Have Been Added To Teamus Support"
     end
