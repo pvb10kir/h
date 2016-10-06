064cea8a27352112fba50c159e8002e22b73a9a3
3pehrdev <sepehrsalehi0111@gmail.com>
2016-10-06 11:50:25 +0330

    Update setcmd.lua

--- plugins/setcmd.lua
+++ plugins/setcmd.lua
@@ -1,14 +1,36 @@
-local function run(msg, matches)
-if matches[1]:lower() == 'setcmd' and is_momod(msg) then
+local function run(msg, matches, callback, extra)
+if matches[1]:lower() == 'setcmd' and matches[2]  == 'en' then
+        if is_owner(msg) then
             hash = 'cmdset:'..msg.to.id
-            redis:hset(hash, matches[2])
-             return 'دستورات گروه تنظیم شد روی فارسی .'
+            redis:set(hash, matches[2])
+local hash2 = 'group:'..msg.to.id
+    local group_lang = redis:hget(hash2,'lang')
+    if group_lang then
+             return 'دستورات گروه روی انگلیسی تنظیم شد '
+else
+           return 'done cmds set to en'
+        end
   end
 end
+if matches[1]:lower() == 'setcmd' and matches[2]  == 'fa' then
+    if is_owner(msg) then
+            hash = 'cmdset:'..msg.to.id
+            redis:set(hash, matches[2])
+local hash2 = 'group:'..msg.to.id
+    local group_lang = redis:hget(hash2,'lang')
+    if group_lang then
+             return 'دستورات گروه روی  فارسی تنظیم شد '
+else
+           return 'done cmds set to fa'
         end
+  end
+end
+end
 return { 
 patterns = { 
-'^[!/#]setcmd (.*)$',
+'^[!/#]setcmd en$',
+'^[!/#]setcmd fa$',
 },
 run = run,
 }
+end
