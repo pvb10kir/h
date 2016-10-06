acc09407d6f94a0601860f51a0dd3bcefbf1353b
3pehrdev <sepehrsalehi0111@gmail.com>
2016-10-06 13:32:18 +0330

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -1,9 +1,9 @@
+do
 
-local function run(msg, matches)
-       if not is_momod(msg) then
-        return "For moderators only!"
+function run(msg, matches)
+       if not is_owner(msg) then
+        return "For owner only!"
        end
-if matches[1] == 'linkpv' then
     local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
@@ -13,17 +13,12 @@ if matches[1] == 'linkpv' then
           send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
            return "SuperGroup link sent on your pv✔"
 end
-if matches[1] == 'newlinkpv' then
-     local data = load_data(_config.moderation.data)
-    local group_link = data[tostring(msg.to.id)]['settings']['set_link']
-send_large_msg('user#id'..msg.from.id, 'New link ;\n'..group_link..'\n@Sphero_Ch', ok_cb, false)
-return 'Supergroup Link has Been revoked And Send to your Private'
-end
+
 return {
   patterns = {
-    "^[/#!]([Ll]inkpv)$",
-    "^[/#!]([Nn]ewlinkpv)$",
+    "^[/#!]([Ll]inkpv)$"
   },
-  run = run,
+  run = run
 }
+
 end
