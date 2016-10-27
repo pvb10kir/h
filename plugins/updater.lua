5688ad630981d5c5c3b53c8fa8cd8d85be7fdedb
3pehrdev <sepehrsalehi0111@gmail.com>
2016-10-27 20:02:09 +0330

    Update updater.lua

--- plugins/updater.lua
+++ plugins/updater.lua
@@ -1,3 +1,7 @@
+local function reload_plugins( )
+	plugins = {}
+  return load_plugins()
+end
 function run(msg, matches)
 if matches[1] == 'update' and is_sudo(msg) then
 local text = io.popen("git pull "):read('*all')
