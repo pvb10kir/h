246f9242e8c1ee31ec107fc0c91fe594f6e03073
3pehrdev <sepehrsalehi0111@gmail.com>
2016-10-27 18:32:48 +0330

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,23 +1,24 @@
-do function run(msg, matches)
-   local hash = 'groupcmds:'..msg.to.id
-    local group_cmds = redis:hget(hash,'cmds')
-if matches[1]:lower() == 'ایدی' or 'id' then
-local hash = 'group:'..msg.to.id
+function run(msg, matches)
+  if matches[1]:lower() == 'ایدی' or 'id' then
+    local hash = 'group:'..msg.to.id
     local group_lang = redis:hget(hash,'lang')
     if group_lang then
-if redis:get("id:"..msg.to.id..":"..msg.from.id) then
-return redis:get("id:"..msg.from.id..":"..msg.from.id)
+      if redis:get("id:"..msg.to.id..":"..msg.from.id) then
+    local wtf = redis:ttl("id:"..msg.to.id..":"..msg.from.id)
+          send_api_msg(msg, get_receiver_api(msg), wtf..' ثانیه مونده', true, 'md')
+      else
+    redis:setex("id:"..msg.to.id..":"..msg.from.id, 60, true)
+    local text = "["..msg.from.print_name.."](https://Telegram.Me/"..(msg.from.username or 'sphero_ch')..")\n*lایدی شماl* : _"..msg.from.id.."_\n*lایدی گروهl :* _"..msg.to.id.."_"
+          send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+    else
+    redis:setex("id:"..msg.to.id..":"..msg.from.id, 60, true)
+    local text = "["..msg.from.print_name.."](https://Telegram.Me/"..(msg.from.username or 'sphero_ch')..")\n*Your *#*ID* : _"..msg.from.id.."_\n*Group *#*ID :* _"..msg.to.id.."_"
+          send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+        end
+     end
+   end
 end
-redis:setex("id:"..msg.to.id..":"..msg.from.id, 60, true)
-local text = "["..msg.from.print_name.."](https://Telegram.Me/"..(msg.from.username or 'sphero_ch')..")\n*lایدی شماl* : _"..msg.from.id.."_\n*lایدی گروهl :* _"..msg.to.id.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-else
-redis:setex("id:"..msg.to.id..":"..msg.from.id, 60, true)
-local text = "["..msg.from.print_name.."](https://Telegram.Me/"..(msg.from.username or 'sphero_ch')..")\n*Your *#*ID* : _"..msg.from.id.."_\n*Group *#*ID :* _"..msg.to.id.."_"
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
-end
-end
+
 return {
 description = "show your id",
 usage = {
@@ -26,9 +27,7 @@ moderator = {
 },
 patterns = {
 "^[!/#]([Ii][Dd])$",
-"^ایدی$",
--- "^[Ii][Dd]$", --For lock cmds
+"^ایدی$", 
 },
 run = run
 }
-end
