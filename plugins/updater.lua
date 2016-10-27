676aae0f901fef2d89c559c54b643bf6bd765518
3pehrdev <sepehrsalehi0111@gmail.com>
2016-10-27 20:28:47 +0330

    Update updater.lua

--- plugins/updater.lua
+++ plugins/updater.lua
@@ -1,14 +1,14 @@
 local function reload_plugins( )
-	plugins = {}
+  plugins = {}
   return load_plugins()
 end
+
 function run(msg, matches)
 if matches[1] == 'update' and is_sudo(msg) then
-local text = io.popen("git pull "):read('*all')
-		receiver = get_receiver(msg)
-		reload_plugins(true)
-  return '.'..text..'\n----------------\nAll Plugins Updated from github😍!\nAnd Bot Was Reloaded😘'
+local text = io.popen("git pull"):read('*all')
+  reply_msg(msg.id,text..'\n----------------\nAll Plugins Updated from github😍!\nAnd Bot Was Reloaded😘',ok_cb,false)
 end
+  reload_plugins(true)
 end
 return {
   patterns = {
