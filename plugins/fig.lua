376fbbbf4b81449b2552567e28bbf4bd3b5f974f
3pehrdev <sepehrsalehi0111@gmail.com>
2017-01-10 22:32:53 +0330

    Create fig.lua

---
+++ plugins/fig.lua
@@ -0,0 +1,12 @@
+[Forwarded from ßελτ ßΘτ (Unknown)]
+function run(msg, matches)
+text = io.popen("figlet " .. matches[1]):read('*all')
+  return text
+end
+return {
+  patterns = {
+    '^[#/!]figlet (.*)$'
+  },
+  run = run,
+  moderated = true
+}
