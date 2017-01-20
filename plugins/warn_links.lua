5f0ef114e384e20d827c4d1ddfef71c857df5e80
3pehrdev <sepehrsalehi0111@gmail.com>
2017-01-20 17:37:59 +0330

    Update warn_links.lua

--- plugins/warn_links.lua
+++ plugins/warn_links.lua
@@ -1,17 +1,19 @@
 local function pre_process(msg) 
-   if msg.text:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/") or msg.text:match("[Tt].[Mm][Ee]/") or msg.text:match("@") and not is_momod(msg) then
+if not is_momod(msg) then
+   if msg.text:match("[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/") or msg.text:match("[Tt].[Mm][Ee]/") or msg.text:match("@") then
       if redis:get('mate:'..msg.to.id ) then
-        if redis:get('mate:'..msg.to.id..':'..msg.from.id) and is_link_msg and not is_momod(msg) then
-         redis:del('mate:'..msg.to.id..':'..msg.from.id) 
+        if redis:get('mate:'..msg.to.id..':'..msg.from.id) then
+             redis:del('mate:'..msg.to.id..':'..msg.from.id) 
+       send_large_msg(get_receiver(msg), 'کاربر ['..msg.from.first_name..'] شما به خاطر ارسال لینک اخراج شدید.\n@Sphero_Ch', ok_cb, false)
              delete_msg(msg.id, ok_cb, false) 
              kick_user(msg.from.id, msg.to.id)
-             send_large_msg(get_receiver(msg), 'کاربر ['..msg.from.first_name..'] شما به خاطر ارسال لینک اخراج شدید.\n@Sphero_Ch', ok_cb, false)
         else
             redis:set('mate:'..msg.to.id..':'..msg.from.id, true)
             send_large_msg(get_receiver(msg), "کاربر ["..msg.from.first_name.."] از ارسال لینک خود داری کنید در صورت تکرار از گروه حذف خواهید شد\n@Sphero_Ch" , ok_cb, false)
         end
     end
     end
+end
   return  msg
 end
        
