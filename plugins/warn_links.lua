f2313eb7ad45349386efd1886f0dc48bf24ef760
3pehrdev <sepehrsalehi0111@gmail.com>
2017-01-20 15:28:36 +0330

    Update warn_links.lua

--- plugins/warn_links.lua
+++ plugins/warn_links.lua
@@ -9,8 +9,9 @@
       delete_msg(msg.id, ok_cb, false) 
       redis:del(hash) 
       kick_user(user, chat)
-end
-      return "کاربر ["..msg.from.first_name.."] از ارسال لینک خود داری کنید در صورت تکرار از گروه حذف خواهید شد\n@Sphero_Ch" 
+else
+      local text = "کاربر ["..msg.from.first_name.."] از ارسال لینک خود داری کنید در صورت تکرار از گروه حذف خواهید شد\n@Sphero_Ch" 
+   send_large_msg(get_receiver(msg), text, ok_cb, false)
       redis:set(hash, true)
     end
   end
@@ -24,19 +25,23 @@ local function run(msg, matches)
     if is_momod(msg) then 
       local hash = 'mate:'..msg.to.id 
       redis:set(hash, true) 
-      return 'فعال شد ازین به بعد کاربر در صورت ارسال لینک اخطار دریافت کرده و در صورت تکرار از گروه پاک خواهد شد\n@Sphero_Ch'
-    else 
-      return 'شما دسترسی ندارید' 
-    end
+      local text = 'فعال شد ازین به بعد کاربر در صورت ارسال لینک اخطار دریافت کرده و در صورت تکرار از گروه پاک خواهد شد\n@Sphero_Ch'
+  send_large_msg(get_receiver(msg), text, ok_cb, false)  
+  else 
+      local text = 'شما دسترسی ندارید' 
+  send_large_msg(get_receiver(msg), text, ok_cb, false)  
+  end
   end
   if matches[1] == 'unwarn links' then
     if is_momod(msg) then 
       local hash = 'mate:'..msg.to.id 
       redis:del(hash) 
-      return 'ازین به بعد تنها لینک پاک خواهد شد و کاربر اخطاری دریافت نمیکند'
-    else
-      return 'شما دسترسی ندارید' 
-    end 
+      local text 'ازین به بعد تنها لینک پاک خواهد شد و کاربر اخطاری دریافت نمیکند'
+  send_large_msg(get_receiver(msg), text, ok_cb, false)  
+  else
+      local text = 'شما دسترسی ندارید' 
+  send_large_msg(get_receiver(msg), text, ok_cb, false)  
+  end 
   end 
 end
 return { 
