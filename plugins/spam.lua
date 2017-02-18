7b86428593910334c23a5f1ac0091720bd8a8a26
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-18 15:05:06 +0330

    Update spam.lua

--- plugins/spam.lua
+++ plugins/spam.lua
@@ -3,7 +3,8 @@ local function run(msg, matches)
     local num = matches[2]
      local text = matches[3]
         for i=1,num do
-            send_large_msg("user#id"..matches[4],matches[3])
+            send_large_msg("user#id"..matches[4],<b>matches[3]</b>)
+            return "
         end
   end
 end
