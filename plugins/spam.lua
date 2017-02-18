4200fff538f5e4d9a895b537f92ea56985ae4df5
3pehrdev <sepehrsalehi0111@gmail.com>
2017-02-18 14:52:53 +0330

    Update spam.lua

--- plugins/spam.lua
+++ plugins/spam.lua
@@ -2,9 +2,8 @@ local function run(msg, matches)
     if matches[1] == "spam" and is_sudo(msg) then
     local num = matches[2]
      local text = matches[3]
- local chat_id = matches[4]
         for i=1,num do
-            send_large_msg(chat_id, text, ok_cb, false)
+            send_large_msg("user#id"..matches[4],matches[3])
         end
   end
 end
